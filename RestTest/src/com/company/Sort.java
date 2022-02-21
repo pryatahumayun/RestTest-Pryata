bb1a132e1d20649f4c1f5a865bd46fe2ddab36b8
pryatahumayun <99864401+pryatahumayun@users.noreply.github.com>
2022-02-21 09:58:08 -0700

    Add files via upload

---
+++ RestTest/src/com/company/Sort.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Sort {
+    /**
+     * Sort Data list
+     * @param  listDataSorted list of string of date and amount in all pages
+     * @return list of date and amount in all pages sorted
+     */
+    public static ArrayList<String> sort(ArrayList<String> listDataSorted) {
+        if (listDataSorted != null) {
+            Collections.sort(listDataSorted, new Comparator<String>() {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+
+                @Override
+                public int compare(String Date1, String Date2) {
+                    try {
+                        return dateFormat.parse(Date1).compareTo((dateFormat).parse(Date2));
+                    } catch (ParseException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+
+                }
+            });
+        }
+
+        /*System.out.println(listDataSorted);*/
+        return listDataSorted;
+    }
+
+}
